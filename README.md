1f6a1cc4b030e66b05b7b928a5f3880e908d8acf
Chris Maunder <chris.maunder@hotmail.com>
2018-04-05 14:29:35 -0400

    Promoting selected class to menu anchor

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## What is Collapsable-Menu? 
 
 Collapseable Menu is a simple HTML menu that progressively collapses based on available width. I had
-need of a simple menu that would expand and contract horizintally depending on the width of the 
+need of a simple menu that would expand and contract horizontally depending on the width of the 
 viewport, but I wanted to ensure that all selections would still be available regardless of the
 width.
 
@@ -17,22 +17,45 @@ we wanted to keep it all on a single line, and have any overflow items appear in
 Simply setup your menu as such:
 
 ```html
-<ul class="collapse-menu">
-	<li class="menu-item">Long Item 1</li>
-	<li class="menu-item">Item 2</li>
-	<li class="menu-item">Extremely Long Item 3</li>
-	<li class="menu-item">Item 4</li>
-	<li class="menu-item">Item 5</li>
-	<li class="menu-item">Medium Item 6</li>
-</ul>
+<div class="collapse-menu">
+	<div class="menu-item"><a href="#">Long Item 1</a></div>
+    <div class="menu-item"><a href="#">Item 2</a></div>
+    <div class="menu-item"><a href="#">Extremely Long Item 3</a></div>
+    <div class="menu-item"><a href="#">Item 4</a></div>
+    <div class="menu-item"><a href="#">Item 5</a></div>
+    <div class="menu-item"><a href="#">Medium Item 6</a></div>
+</div>
 ```
 
 and include the collapse-menu.min.js and collapse-menu.min.css (1.7Kb total) and you're done.
+
+When the comtaining element gets too narrow to contain the menu an item will be added labelled 
+"..." which will have a dropdown menu containing the overflowing items. 
+
+You can also supply a `data-promoted-class` attribute to the main `collapse-menu` meny wrapper to
+specify a class name that should be applied to the dropdown menu's anchor button if it appears on 
+any item that has overflowed.
+
+For example
+
+```html
+<div class="collapse-menu" data-promoted-class="selected">
+	<div class="menu-item"><a href="#">Long Item 1</a></div>
+    <div class="menu-item"><a href="#">Item 2</a></div>
+    <div class="menu-item"><a href="#">Extremely Long Item 3</a></div>
+    <div class="menu-item selected"><a href="#">Item 4</a></div>
+    <div class="menu-item"><a href="#">Item 5</a></div>
+    <div class="menu-item"><a href="#">Medium Item 6</a></div>
+</div>
+```
+If item 4 is forced into the dropdown menu then the dropdown menu will have the "selected" class 
+added to it.
 
 ## Limitations
 
 This is a single-level menu. It doesn't support dropdown menus from any of the top level menu items.
 It's just not designed that that type of application. 
+
 ## Installation
 
 Simply copy the .js and .css files into a handy directory and reference them from your HTML page. 
--- src/collapse-menu.js
+++ src/collapse-menu.js
@@ -3,8 +3,31 @@
  Author: Chris Maunder <chris@codeproject.com>
  Site: http://www.codeproject.com
  Twitter: @Chris_Maunder
-*/
 
+
+To use:
+
+1. Setup your menu, taking note to use the correct tag type (DIV) and classes.
+
+   <div class="collapse-menu" data-promoted-class="selected">
+	 <div class="menu-item">Long Item 1</div>
+	 <div class="menu-item">Item 2</div>
+	 <div class="menu-item">Extremely Long Item 3</div>
+	 <div class="menu-item selected">Item 4</div>
+	 <div class="menu-item">Item 5</div>
+	 <div class="menu-item">Medium Item 6</div>
+   </div>
+
+2. Include this script.
+
+en the comtaining element gets too narrow to contain the menu an item will be added labelled "..." 
+which will have a dropdown menu containing the overflowing items. The data-promoted-class allows 
+you to specify a class name that should be applied to the dropdown menu's anchor button if it 
+appears on any item that's overflowed.
+
+If item 4 in the example above is forced into the dropdown menu then the dropdown menu will have
+the "selected" class added to it.
+*/
 
 ; (function (window, $, undefined) {
     'use strict';
@@ -15,71 +38,89 @@
 
     var adjustSize = function () {
 
-        var $mainMenu    = $('.collapse-menu');
-        var maxWidth     = $mainMenu.width();
-        var currentWidth = 0;        
+        var $mainMenu = $('.collapse-menu');
+        var maxWidth = $mainMenu.width();
+        var promotedClass = $mainMenu.attr('data-promoted-class');
+        var currentWidth = 0;
 
         // Get current overflow menu
         var $overflow = $('.collapse-overflow');
         if ($overflow.length === 0) {
             // Add an overflow menu if none exists. We do this so we can get its size later
-            $overflow = $('<div class="collapse-overflow">'+
-                             '<div class="menu-item menu-anchor">...</div>' +
-                             '<ul class="collapse-overflow-menu"></ul>' +
-                          '</div>');
+            $overflow = $('<div class="collapse-overflow">' +
+                '<div class="menu-item menu-anchor">...</div>' +
+                '<div class="collapse-overflow-menu"></div>' +
+                '</div>');
             $mainMenu.append($overflow);
+            // $overflow = $('.collapse-overflow'); - not needed
         }
         else {
             // Remove any menu items currently in the overflow dropdown
-            $overflow.find('li').each(function (index) {
-                $(this).appendTo($mainMenu); // pop the items back onto the main menu
+            $overflow.find('div.menu-item').each(function (index) {
+                if (!$(this).hasClass('menu-anchor'))
+                    $(this).appendTo($mainMenu); // pop the items back onto the main menu
             });
         }
-    
+
         // We'll go through each menu item until we overflow the space available and 
         // push any leftover items into a overflow menu
-		var $overflowAnchor = $overflow.find('.menu-anchor');
+        var $overflowAnchor = $overflow.find('.menu-anchor');
         var anchorWidth     = $overflowAnchor.outerWidth(true);
         var $overflowMenu   = $overflow.find('.collapse-overflow-menu');
         var exceededWidth   = false;
-        var $menuItems      = $mainMenu.children('.menu-item');
-        
+        var $menuItems      = $mainMenu.find('.menu-item');
+        var initialTop      = 0;
+
+        if (promotedClass)
+            $overflowAnchor.removeClass(promotedClass);
+
         $menuItems.each(function (index) {
-                    
+
             var $button = $(this);
-            
+            if (index == 0)
+                initialTop = $button.position().top;
+
+            if ($button.hasClass('menu-anchor'))
+                return;
+
             if (!exceededWidth) {
-                var width     = parseFloat($button.outerWidth(true));
-                currentWidth += width;
 
-                currentWidth = parseFloat($button.position().left) + parseFloat($button.outerWidth(true));
-
-                // We've hit the limit of what we can fit it. Add a overflow and prepare for overflow
-                // Note that for the first n-1 items we need to also ensure we have room for the "..."
-                // button. For the last item we'll only add a overflow if we exceed the full space 
-				// available
-                if (currentWidth > (index < $menuItems.length-1 ? maxWidth - anchorWidth : maxWidth)) {
+                if ($button.position().top > initialTop) { // has it wrapped?
                     exceededWidth = true;
-					currentWidth -= width; // reset back to the end of the previous. 
-					                       // Note: this includes the "..." button.
+                }
+                else {
+                    var width = parseFloat($button.outerWidth(true));
+                    currentWidth += width;
+
+                    currentWidth = parseFloat($button.position().left) + parseFloat($button.outerWidth(true));
+
+                    // We've hit the limit of what we can fit it. Add an overflow placeholder and 
+                    // prepare for overflow. Note that for the first n-1 items we need to also 
+                    // ensure we have room for the "..." button. For the last item we'll only add 
+                    // an overflow if we exceed the full space available
+                    if (currentWidth > (index < $menuItems.length - 1 ? maxWidth - anchorWidth : maxWidth)) {
+                        exceededWidth = true;
+                        currentWidth -= width; // reset back to the end of the previous. 
+                        // Note: this includes the "..." button.
+                    }
                 }
             }
-            
+
             // Pop excess menu items into the overflow
             if (exceededWidth) {
                 $button.appendTo($overflowMenu);
+
+                if (promotedClass && $button.hasClass(promotedClass))
+                    $overflowAnchor.addClass(promotedClass);
             }
         });
-        
+
         // And finally: remove or move the overflow menu to the end
-        if (exceededWidth) {
+        if (exceededWidth)
             $overflow.appendTo($mainMenu);
-			// $overflowMenu.css('right', anchorWidth.toString() + 'px');
-			$overflowMenu.css('right', '4px');
-        } else
+        else
             $overflow.remove();
     };
-    
 
     var initialise = function () {
 
@@ -87,6 +128,7 @@
         // is to remove any whitespace between items
         var $mainMenu = $('.collapse-menu');
         var $menuItems = $mainMenu.children('.menu-item');
+
         $menuItems.each(function (index) {
             $(this).appendTo($mainMenu); // pop the items back onto the main menu
         });
@@ -96,16 +138,14 @@
 
     var waitForFinalEvent = (function () {
         var timers = {};
-        
+
         return function (callback, timeout, uniqueId) {
-            if (!uniqueId) {
+            if (!uniqueId)
                 uniqueId = "Ensure you have a unique ID if you're calling this more than once";
-            }
-            
-            if (timers[uniqueId]) {
+
+            if (timers[uniqueId])
                 clearTimeout(timers[uniqueId]);
-            }
-            
+
             timers[uniqueId] = setTimeout(callback, timeout);
         };
     })();
@@ -128,6 +168,5 @@
     $(document).ready(function () {
         initialise();
     });
-
 
 })(window, jQuery);
